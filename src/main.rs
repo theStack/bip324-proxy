c96cb58f4fbbbc43c0a68ccf7218e0b091b1e582
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-03-16 21:52:06 +0100

    start listening on proxy port 1324, add a bunch of TODOs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::net::TcpListener;
+use std::thread::sleep;
+use std::time::Duration;
+
 const BIP324_PROXY_PORT: u16 = 1324;
 
 fn main() {
+    // TODO: don't use unwrap() here, we want a nice error message if binding
+    // fails (e.g. due to address already in use), instead of an ugly panic
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", BIP324_PROXY_PORT)).unwrap();
     println!("---------------------");
     println!(" BIP324 proxy server ");
     println!("---------------------");
     println!("Waiting for incoming v1 connections on 127.0.0.1:{}...", BIP324_PROXY_PORT);
+    loop {
+        // TODO: terminate if SIGINT (CTRL+C) is received
+        // TODO: accept client connections here
+        sleep(Duration::from_secs(10));
+        break;
+    }
 }
