cb3b0c88205a3e6d69b72284c5eb5c43b8a16060
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-03-16 22:57:14 +0100

    accept client connections, start proxy handler function (empty for now)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use std::net::TcpListener;
-use std::thread::sleep;
-use std::time::Duration;
+use std::net::{TcpListener, TcpStream};
 
 const BIP324_PROXY_PORT: u16 = 1324;
+
+fn bip324_proxy_handler(local_socket: TcpStream) {
+    println!("TODO: implement bip324_proxy_handler")
+}
 
 fn main() {
     // TODO: don't use unwrap() here, we want a nice error message if binding
@@ -12,10 +14,12 @@ fn main() {
     println!(" BIP324 proxy server ");
     println!("---------------------");
     println!("Waiting for incoming v1 connections on 127.0.0.1:{}...", BIP324_PROXY_PORT);
-    loop {
-        // TODO: terminate if SIGINT (CTRL+C) is received
-        // TODO: accept client connections here
-        sleep(Duration::from_secs(10));
-        break;
+
+    // TODO: terminate if SIGINT (CTRL+C) is received
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("[<] New connection from {}", stream.peer_addr().unwrap());
+        // TODO: start the handler in a new thread
+        bip324_proxy_handler(stream);
     }
 }
