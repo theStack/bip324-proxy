db10b64db07437ca38c45b514c9cb9dfc647bb4b
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-03-16 21:27:45 +0100

    define bip324 proxy port constant, show banner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+const BIP324_PROXY_PORT: u16 = 1324;
+
 fn main() {
-    println!("Hello, world!");
+    println!("---------------------");
+    println!(" BIP324 proxy server ");
+    println!("---------------------");
+    println!("Waiting for incoming v1 connections on 127.0.0.1:{}...", BIP324_PROXY_PORT);
 }
