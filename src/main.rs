ae19ed42916175d2f74f989bbd3f60dcab3fef6f
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2024-03-20 00:07:48 +0100

    extract v1 msgtype + payload
    next step: verify checksum

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,22 @@ const NET_MAGIC: [u8; 4] = [0xf9, 0xbe, 0xb4, 0xd9]; // mainnet
 
 fn recv_v1_message(sock: &TcpStream) -> (String, Vec<u8>) {
     let mut buf: Vec<u8> = vec![];
+    let mut payload: Vec<u8> = vec![];
 
     sock.take(24).read_to_end(&mut buf).unwrap();
-    println!("received v1 header bytes: {:02x?}", buf);
     // TODO: proper error handling, don't panic
     assert_eq!(NET_MAGIC, buf[0..4], "network magic mismatch");
-    println!("TODO: extract msgtype + payload");
-    println!("TODO: verify checksum");
-    ("dummymsgtype".to_string(), buf) // TODO
+    let msgtype = String::from_utf8(buf[4..16].to_vec()).unwrap();
+    let payload_len = u32::from_le_bytes(buf[16..20].try_into().unwrap());
+    sock.take(payload_len as u64).read_to_end(&mut payload).unwrap();
+    println!("TODO: verify v1 payload checksum");
+    (msgtype, payload) // TODO
 }
 
 fn bip324_proxy_handler(local_socket: TcpStream) {
-    let (_msgtype, _payload) = recv_v1_message(&local_socket);
-    println!("TODO: implement bip324_proxy_handler")
+    let (msgtype, payload) = recv_v1_message(&local_socket);
+    println!("received msgtype = {}, payload = {:02x?}", msgtype, payload);
+    println!("TODO: implement rest of bip324_proxy_handler")
 }
 
 fn main() {
