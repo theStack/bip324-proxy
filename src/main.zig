55c29a307bfadfd2c4fb84272b1fdbfdde0d8ed1
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-09-28 20:45:41 +0200

    ziggify: create server, accept client connections
    so far so good

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const net = std.net;
 
+const BIP324_PROXY_PORT: u16 = 1324;
+
 var stdout_buf: [1024]u8 = undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
 const stdout = &stdout_writer.interface;
@@ -14,4 +16,15 @@ pub fn main() !void {
     try print("---------------------\n", .{});
     try print(" BIP324 proxy server \n", .{});
     try print("---------------------\n", .{});
+
+    const server_addr = try net.Address.parseIp4("127.0.0.1", BIP324_PROXY_PORT);
+    var server = try server_addr.listen(.{.reuse_address = true});
+    defer server.deinit();
+    try print("Waiting for incoming v1 connections on {f}...\n", .{server.listen_address});
+
+    while (true) {
+        const client = try server.accept();
+        defer client.stream.close();
+        try print("[<] New connection from {f}\n", .{client.address});
+    }
 }
