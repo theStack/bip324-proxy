639077d03c9cc97d926a744b7193e9aea809dc00
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2025-09-28 23:49:18 +0200

    ziggify: add proxy handler skeleton, detect v1 prefix

--- src/main.zig
+++ src/main.zig
@@ -15,6 +15,28 @@ fn print(comptime fmt: []const u8, args: anytype) !void {
     try stdout.flush();
 }
 
+fn bip324ProxyHandler(client: *const net.Server.Connection) !void {
+    // peek into receiver buffer byte for byte to detect early if the first
+    // incoming message is not a bitcoin p2p v1 message; in that case we can't
+    // do anything (we wouldn't know the remote destination to send data to) and
+    // have to close the local connection
+    var received_prefix: [16]u8 = undefined;
+
+    for (0..V1_PREFIX.len) |i| {
+        var byte_buf: [1]u8 = undefined;
+        const n_read = try client.stream.read(&byte_buf);
+        if (n_read == 0) return error.ConnectionClosed;
+        const byte = byte_buf[0];
+        try print("byte read: {d}\n", .{byte});
+        if (byte != V1_PREFIX[i]) {
+            try print("V1 prefix mismatch after {d} bytes, close connection.\n", .{i+1});
+            // TODO: show expected/received byte-strings
+            return error.ConnectionClosed; // TODO: right error code?
+        }
+        received_prefix[i] = byte;
+    }
+}
+
 pub fn main() !void {
     try print("---------------------\n", .{});
     try print(" BIP324 proxy server \n", .{});
@@ -29,5 +51,7 @@ pub fn main() !void {
         const client = try server.accept();
         defer client.stream.close();
         try print("[<] New connection from {f}\n", .{client.address});
+        // TODO: start this up in a new thread
+        try bip324ProxyHandler(&client);
     }
 }
